efe3e7056a8abaf6e58e21b297693545a9c6eff3
Tom Collins <tomascollins@gmail.com>
2022-01-03 12:06:23 +0000

    update photo

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,15 +3,16 @@
 		letter-spacing: 6px;
 		background-size: cover;
 		background-color: black;
-		background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
-			url('/hair-2.jpg');
-		text-shadow: 1px 1px rgb(88, 88, 88);
+		background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+			url('/salon-waiting.jpeg');
+		text-shadow: 1px 1px rgb(77, 77, 77);
+		background-position: 50% 10%;
 		font-family: Italiana, Candara, Calibri, Segoe, Segoe UI, Optima, Arial, sans-serif;
 	}
 </style>
 
 <div class="header flex items-center justify-center w-full min-h-[50vh] md:min-h-[70vh]">
-	<!-- <h1 class="py-5 px-5 md:text-8xl text-5xl text-red-500 m-3 borderborder border-gray-300"></h1> -->
+	<!-- <h1 class="py-5 px-5 md:text-9xl text-5xl text-red-500 m-3 border border-gray-300"> -->
 	<h1 class="text-5xl text-red-500 m-3 md:text-9xl ">
 		ROSE<span class="text-gray-300">HAIR</span>CO
 	</h1>
